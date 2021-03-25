8e0024cb7a08eded5e8dfdcf68decae999b6c137
Dawit Michael <dave.mic1227@gmail.com>
2021-03-25 20:06:00 +0300

    .

--- MVC_Assignment/Controllers/HomeController.cs
+++ MVC_Assignment/Controllers/HomeController.cs
@@ -13,17 +13,24 @@ namespace MVC_Assignment.Controllers
         {
             return View();
         }
-        public ActionResult Gallery()
+        public ActionResult Products()
         {
+            var images = new string[3] { "../images/amazon-echo-dot-4th-gen.png", "../images/echo-dot-with-clock-4th-gen-2020-official-render.png", "../images/amazon-echo-dot-4th-gen-smart-speaker-w-alexa-w-clock-w-alexa-glacier-white-d-b07xj8c8f7.png", };
+            var colors = new string[3] { "Black", "Gray", "White" };
+            var prices = new Int16[3] { 220, 160, 200 };
+            ViewBag.Images = images;
+            ViewBag.Colors = colors;
+            ViewBag.Prices = prices;
             return View();
         }
         public ActionResult About()
         {
             return View();
         }
-        public ActionResult Contact()
+        public ActionResult Amazon()
         {
-            return View();
+            var url = "http://www.Amazon.com";
+            return Redirect(url);
         }
         public ActionResult Download()
         {
