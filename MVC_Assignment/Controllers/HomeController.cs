9135728dd63426434ea536e12a981900494fb314
Dawit Michael <dave.mic1227@gmail.com>
2021-03-25 18:44:08 +0300

    Add project files.

---
+++ MVC_Assignment/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Assignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Download()
+        {
+            var file = "../documents/Using_random_forests_V3.1.pdf";
+            return File(file, contentType: "Application/pdf");
+        }
+
+    }
+}
